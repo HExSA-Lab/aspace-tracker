6c06f45494e444083555ca766a643fee2bb488a3
Kyle C. Hale <khale@cs.iit.edu>
2017-05-04 16:18:28 -0500

    zero pages stored in a hashtable, we're now tracing the target for sigs

--- Makefile
+++ Makefile
@@ -5,19 +5,25 @@ OBJS:=zpage test
 
 all: kzpage.ko zpage 
 
-zpage: zpage.c libpt_scan.a
-	$(CC) $(MCFLAGS) -L. -lpt_scan -o $@ $< -lpt_scan
+zpage.o: zpage.c
+	$(CC) $(MCFLAGS) -o $@ -c $<
+
+hashtable.o: hashtable.c hashtable.h
+	$(CC) $(MCFLAGS) -o $@ -c $<
+
+zpage: zpage.o hashtable.o libpt_scan.a 
+	$(CC) -L. -lpt_scan -o $@ zpage.o hashtable.o -lpt_scan
 
 obj-m+=kzpage.o
 
 libpt_scan.a: pt_scan.o
-	$(AR) ruv libpt_scan.a pt_scan.o
+	$(AR) rv libpt_scan.a pt_scan.o
 
 pt_scan.o: pt_scan.c pt_scan.h
 	$(CC) $(MCFLAGS) -static -c pt_scan.c
 
 test: test.c
-	$(CC) $(MCFLAGS) test.c
+	$(CC) -Wall -o $@ $<
 
 kzpage.ko: kzpage.c
 	$(MAKE) -C /lib/modules/`uname -r`/build M=$(PWD) modules
--- zpage.c
+++ zpage.c
@@ -4,22 +4,48 @@
  * (c) Kyle C. Hale 2017
  * Illinois Institute of Technology
  *
- *
- * Relies on: libnl 3.1 for netlink socket access
  */
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <signal.h>
+#include <wait.h>
 
 #include <sys/mman.h>
+#include <sys/ptrace.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 
 #include "kzparms.h"
 #include "pt_scan.h"
+#include "hashtable.h"
+
+pid_t tracee;
+
+#define DEBUG 1
+
+#if DEBUG==1
+#define DEBUG_PRINT(fmt, args...) printf("<DEBUG> " fmt, ##args)
+#else 
+#define DEBUG_PRINT(fmt, args...)
+#endif
+
+
+typedef unsigned long addr_t;
+
+typedef enum {
+    ZP_INIT, /* zero page was present at the beginning */
+    ZP_DYN,  /* this zpage entry was mapped at run time */
+} zp_type_t;
+
+
+struct zpinfo {
+    zp_type_t type;
+    uint64_t vaddr;
+    unsigned refcnt;
+};
 
 
 static void
@@ -27,6 +53,19 @@ usage (char ** argv)
 {
     fprintf(stderr, "%s <pid>\n", argv[0]);
     exit(0);
+}
+
+
+static unsigned
+zpage_hash_fn (addr_t key)
+{
+    return v3_hash_long(key, sizeof(void*));
+}
+
+static int
+zpage_eq_fn (addr_t key1, addr_t key2)
+{
+    return (key1 == key2);
 }
 
 
@@ -111,41 +150,141 @@ cleanup_msg (struct msghdr * msg)
 }
 
 
-static int 
+static struct hashtable*
 find_all_zpages (int pid)
 {
     uint64_t npages = 0;
     uint64_t zeros = 0;
     struct page_info *pi = NULL;
 
+    struct hashtable *h = v3_create_htable(0, zpage_hash_fn, zpage_eq_fn);
+    if (!h) {
+        fprintf(stderr, "Could not create zpage hashtable\n");
+        return NULL;
+    }
+
     if (pt_scan_get_page_infos_all_maps(pid, NULL, &npages)) {
         fprintf(stderr, "Failed to get number of pages\n");
-        return -1;
+        return NULL;
     }
 
     pi = (struct page_info*)malloc(sizeof(struct page_info)*npages);
     if (!pi) {
         fprintf(stderr, "Could not allocate page info array\n");
-        return -1;
+        return NULL;
     }
     memset(pi, 0, sizeof(struct page_info)*npages);
 
     if (pt_scan_get_page_infos_all_maps(pid, pi, &npages)) {
         fprintf(stderr, "Could not get pages\n");
         free(pi);
-        return -1;
+        return NULL;
     }
 
     printf("Successfully scanned pages\n");
 
     for (int i = 0; i < npages; i++) {
         if (pi[i].flags.flags.ZERO_PAGE == 1) {
+
+            /* create a new entry */
+            struct zpinfo * zp = malloc(sizeof(struct zpinfo));
+            if (!zp) {
+                fprintf(stderr, "Could not create zero page entry\n");
+                return NULL;
+            }
+            memset(zp, 0, sizeof(struct zpinfo));
+
+            zp->type   = ZP_INIT;
+            zp->vaddr  = pi[i].va;
+            zp->refcnt = pi[i].refcount;
+
+            /* stash it */
+            addr_t ret = v3_htable_insert(h, 
+                                          (addr_t)zp->vaddr, 
+                                          (addr_t)zp);
+
+            if (!ret) {
+                fprintf(stderr, "Could not insert zero page entry into hashtable\n");
+                return NULL;
+            }
+
+            /* KCH HERE */
             zeros++;
         }
     }
 
+    printf("Found %lu zero page references (%lu scanned)\n", zeros, npages);
 
-    printf("Found %lu zero page references (%d scanned)\n", zeros, npages);
+    return h;
+}
+
+
+static void
+die (int dontcare)
+{
+    printf("User interrupt...detaching from child\n");
+    ptrace(PTRACE_DETACH, tracee, 0, 0);
+    exit(-1);
+}
+
+
+/*
+ * waits for tracee to change state. Returns when it does, will
+ * die if child exits
+ *
+ */
+static void
+waitonit (int pid)
+{
+    int status;
+    pid_t pw = wait(&status);
+
+    if (pw == (pid_t) -1) {
+        perror("issue waiting for tracee");
+        exit(1);
+    }
+
+    if (WIFEXITED(status)) {
+        fprintf(stderr, "Child exited with status %d\n",
+                WEXITSTATUS(status));
+    }
+
+    DEBUG_PRINT("Tracee got signal (%d)\n", WSTOPSIG(status));
+
+    if (WSTOPSIG(status) != SIGSTOP) {
+        DEBUG_PRINT("passing signal along\n");
+        ptrace(PTRACE_SYSCALL, pid, NULL, WSTOPSIG(status));
+    }
+
+    if (WSTOPSIG(status) == SIGKILL ||
+        WSTOPSIG(status) == SIGINT) {
+        // we're done here
+        printf("Child received signal, exiting.\n");
+        exit(0);
+    }
+        
+}
+
+
+static void
+cont (int pid)
+{
+    ptrace(PTRACE_CONT, pid, 0, 0);
+}
+
+
+static int 
+trace (int pid)
+{
+    long ret = ptrace(PTRACE_ATTACH, pid, NULL, NULL);
+
+    if (ret < 0) {
+        perror("Could not trace child process\n");
+        return -1;
+    }
+
+    /* wait for it to stop */
+    waitonit(pid);
 
     return 0;
 }
@@ -158,9 +297,9 @@ main (int argc, char * argv[])
     int sock_fd;
     struct sockaddr_nl src_addr, dst_addr;
     struct msghdr* m = NULL;
+    struct hashtable *htable;
 
     char * test = "Here is a test";
-
 
     /* exit */
     if (argc != 2) {
@@ -202,9 +341,33 @@ main (int argc, char * argv[])
 
     pid = atoi(argv[1]);
 
+    tracee = pid;
+
     printf("Starting zpage daemon (tracking pid %d)\n", pid);
 
-    find_all_zpages(pid);
+    if (trace(pid) != 0) {
+        fprintf(stderr, "Could not trace process\n");
+        return -1;
+    }
+
+    /* we should detach if we die */
+    signal(SIGINT, die);
+
+    /* while it's stopped, find the current zero pages */
+    htable = find_all_zpages(pid);
+
+    if (!htable) {
+        fprintf(stderr, "Error finding zero pages\n");
+        exit(1);
+    }
+
+    /* continue the process */
+    cont(pid);
+
+    /* let the thing go */
+    while (1) {
+        waitonit(pid);
+    }
 
     cleanup_msg(m);
 
